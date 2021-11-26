5bbbdbc0110b74d9bbd7286e617329e1b34d6266
vaibhavpriyatosh <90557665+vaibhavpriyatosh@users.noreply.github.com>
2021-11-26 21:19:30 +0530

    POTD:26/11/2021

---
+++ PROBABLITY/money_distributions.java
@@ -0,0 +1,40 @@
+class money_didtributions{
+	public long power(long a,long b , long mod){
+        
+        long ans = 1L;
+        while(b>0){
+            if((b&1)!=0 ){
+                ans = (ans*a)%mod;
+            }
+            a = (a*a)%mod;
+            b>>=1;
+        }
+        return ans;
+    }
+    
+    public int nCr(int N,int R){
+        
+        long res= 1L;
+        //long deno=1L;
+        if(R > N-R){
+            R = N-R;
+        }
+        long mod =(long) 1e9+7;
+        for(int i=0;i<R;i++){
+            
+            res = (res *(N-i))%mod;
+            res= res*power(i+1 , mod-2 ,mod)%mod;
+            
+        }
+        
+        return (int)res;
+    }
+    public int totalWays(int N, int K)
+    {
+        if(K > N)return 0;
+        if(K==N)return 1;
+        
+        return nCr(N-1,K-1);
+        
+    }
+}
