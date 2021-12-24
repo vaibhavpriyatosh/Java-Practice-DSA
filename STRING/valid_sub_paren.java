ef20cbcc392b1c5a97cb742b39659129b98deee2
vaibhavpriyatosh <90557665+vaibhavpriyatosh@users.noreply.github.com>
2021-12-24 08:05:58 +0530

    POTD:24/12/2021

---
+++ STRING/valid_sub_paren.java
@@ -0,0 +1,36 @@
+class valid_sub_paren
+{
+static int findMaxLen(String s) {
+         int n = s.length();
+
+        int left = 0, right = 0, maxlength = 0;
+
+        for (int i = 0; i < n; i++) {
+            
+            if (s.charAt(i) == '(')
+                left++;
+            else
+                right++;
+                
+            if (left == right) maxlength = Math.max(maxlength, 2 * right);
+
+            else if (right > left)
+                left = right = 0;
+        }
+
+        left = right = 0;
+
+        for (int i = n - 1; i >= 0; i--) {
+
+            if (s.charAt(i) == '(')
+                left++;
+            else
+                right++;
+
+            if (left == right) maxlength = Math.max(maxlength, 2 * left);
+
+            else if (left > right)
+                left = right = 0;
+        }
+        return maxlength;
+    }}
